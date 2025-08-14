0ce8cec527bbd45238ee5dc8232b774899706ebe
sounest <222609192+sounest@users.noreply.github.com>
2025-08-14 11:54:10 +0530

    Update Loginartist.tsx

--- src/Components/Form/Loginartist.tsx
+++ src/Components/Form/Loginartist.tsx
@@ -32,7 +32,7 @@ const Loginartist = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/artist/login", {
+      const res = await fetch("https://soundest-backend.vercel.app/api/artist/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: user.Email, password: user.Password }),
